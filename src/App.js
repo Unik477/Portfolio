031f642ffb237e73935fd57838ff1a51e649c6e8
Unik477 <nikhilupadhyay477@gmail.com>
2024-07-22 20:53:52 +0530

    mess resolved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 // import Navbar from './Components/Navbar/Navbar';
-
-import Home from './Components/Home/heroSection';
+// import { Router,Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/heroSection";
 // import Education from './Components/Education/education';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/footer';
-import About from './Components/About/about';
-import Portfolio from './Components/Portfolio/portfolio';
-import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/footer";
+import About from "./Components/About/about";
+import Portfolio from "./Components/Portfolio/portfolio";
+import "./App.css";
 
 function App() {
-
-  
   return (
     <>
-  <Navbar/>
-  <Home/>
-  {/* <Education/> */}
-  <Portfolio/>
-    <About />
-  <Footer/>
+      <Navbar />
+      <Home />
+      <Portfolio />
+      <About />
+      <Footer />
+    
     </>
   );
 }
